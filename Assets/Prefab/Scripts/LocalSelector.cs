98f3bdf53d3bb7b5c4a705b4e8ee3637380e9c2b
FatihSevencan <fatihsevencan@hotmail.com>
2023-03-22 10:56:08 +0300

    feat:scripts

---
+++ Assets/Prefab/Scripts/LocalSelector.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization.Settings;
+
+
+public class LocalSelector : MonoBehaviour
+{
+    private void Start()
+    {
+        int ID = PlayerPrefs.GetInt("LocalKey", 0);
+        ChangeLocale(0);
+    }
+
+    private bool active = false;
+    public void ChangeLocale(int localeID)
+    {
+        if(active==true)
+            return;
+        StartCoroutine(SetLocale(localeID));
+        
+    }
+    
+    
+    IEnumerator SetLocale(int _localeID)
+    {
+        active = true;
+        yield return LocalizationSettings.InitializationOperation;
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[_localeID];
+        active = false;
+    }
+}
